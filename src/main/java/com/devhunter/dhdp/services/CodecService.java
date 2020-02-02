c518e2f10cc6a272d86c7f7cd37423ae193e9175
inertBear <31257145+inertBear@users.noreply.github.com>
2020-02-01 20:07:25 -0600

    formatting update

--- src/main/java/com/devhunter/dhdp/services/CodecService.java
+++ src/main/java/com/devhunter/dhdp/services/CodecService.java
@@ -10,6 +10,8 @@ import org.apache.commons.codec.binary.Base64;
 
 import java.util.logging.Logger;
 
+import static com.devhunter.dhdp.infrastructure.DHDPConstants.CODEC_SERVICE_NAME;
+
 /**
  * Encodes and Decodes client communications.
  * This Service is registered with the Service Registry upon startup
@@ -23,7 +25,7 @@ public class CodecService extends DHDPService {
 
     public static void initService(DHDPServiceRegistry registry) {
         if (!registry.containsService(CodecService.class)) {
-            registry.register(CodecService.class, new CodecService("CodecService"));
+            registry.register(CodecService.class, new CodecService(CODEC_SERVICE_NAME));
         }
     }
 
@@ -37,9 +39,10 @@ public class CodecService extends DHDPService {
         // convert
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String responseString = gson.toJson(response, DHDPResponse.class);
-        mLogger.info("DHDP - transmitting response: " + responseString);
         // encode
         byte[] encodedBytes = Base64.encodeBase64(responseString.getBytes());
+
+        mLogger.info("DHDP - transmitting response: " + responseString);
         return new String(encodedBytes);
     }
 
@@ -56,6 +59,7 @@ public class CodecService extends DHDPService {
         String decodedRequest = new String(decodedBytes);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         DHDPRequest request = gson.fromJson(decodedRequest, DHDPRequest.class);
+
         mLogger.info("DHDP - received request: " + gson.toJson(request));
         return request;
     }
